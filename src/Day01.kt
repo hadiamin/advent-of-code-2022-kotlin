aed203cf7305327bfb5352ca9538dae35b252310
amin-io <amin.hadi@turntabl.io>
2022-12-02 05:49:28 +0000

    Day 1: Calorie Counting done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val sumedList = mutableListOf<Int>()
+        var sum = 0
+        for (item in input) {
+            if (item != "") {
+                sum += item.toInt()
+            } else {
+                sumedList.add(sum)
+                sum = 0
+            }
+        }
+
+        return sumedList.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sumedList = mutableListOf<Int>()
+        var sum = 0
+        for (item in input) {
+            if (item != "") {
+                sum += item.toInt()
+            } else {
+                sumedList.add(sum)
+                sum = 0
+            }
+        }
+
+        return sumedList.sorted().reversed().subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01.txt")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
