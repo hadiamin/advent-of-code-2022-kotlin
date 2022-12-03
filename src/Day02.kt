49be183a4e5d0918a471743378eda38f1db89b4c
amin-io <amin.hadi@turntabl.io>
2022-12-03 00:11:01 +0000

    Day 2: part 2 completed

--- src/Day02.kt
+++ src/Day02.kt
@@ -17,7 +17,25 @@ fun main() {
 
         return totalScore
     }
+
+    fun part2(input: List<String>): Int {
+        var totalScore = 0
+        for (item in input) {
+            when (item) {
+                "A X" -> totalScore += 3
+                "B X" -> totalScore += 1
+                "C X" -> totalScore += 2
+                "A Y" -> totalScore += 4
+                "B Y" -> totalScore += 5
+                "C Y" -> totalScore += 6
+                "A Z" -> totalScore += 8
+                "B Z" -> totalScore += 9
+                "C Z" -> totalScore += 7
+            }
+        }
+        return  totalScore
+    }
     val input = readInput("Day02")
-    println(part1(input))
+    println(part2(input))
 
 }
