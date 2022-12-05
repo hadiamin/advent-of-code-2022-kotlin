8b35634c5c23b0a8c96c189c30e62d35600451a7
amin-io <amin.hadi@turntabl.io>
2022-12-05 22:32:34 +0000

    improved Day03 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,11 +1,4 @@
 fun main() {
-    fun Char.priority(): Int {
-        if (this.isUpperCase()) {
-            return this.code - 'A'.code + 27
-        } else {
-            return this.code - 'a'.code + 1
-        }
-    }
 
     fun part1(input: List<String>): Int {
         return input.map {
@@ -16,16 +9,26 @@ fun main() {
 //            .also {
 //                it.forEach { println(it.priority()) }
 //            }
-            .sumOf { it.priority()}
+            .sumOf { it.priority }
     }
 
     fun part2(input: List<String>): Int {
         return input.chunked(3)
+            .also { println(it) }
             .flatMap { (first, second, third) -> first.toSet() intersect second.toSet() intersect third.toSet() }
-            .sumOf { it.priority() }
+            .sumOf { it.priority }
     }
 
 
     val input = readInput("Day03")
     println(part2(input))
 }
+
+val Char.priority: Int
+    get(): Int {
+        return when (this) {
+            in 'a' .. 'z' -> this - 'a' + 1
+            in 'A' .. 'Z' -> this - 'A' + 27
+            else -> error("Check your input")
+        }
+    }
